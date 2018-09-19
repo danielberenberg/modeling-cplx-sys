8e53635d018981abe2171cdf2aec79d69eec3147
danielberenberg <djberenb@uvm.edu>
2018-09-19 13:59:30 -0400

    numint

---
+++ numerical_integration.cpp
@@ -0,0 +1,20 @@
+#include "numerical-integration.hpp"
+#include <functional>
+
+double euler(double ytm1, std::function<double (double)> dydt, double t, double h){
+    /*
+     * compute Euler's method. Given the derivative dy/dt of an unknown function F,
+     * approximate F(t) = y_{t-1} + dy/dt (t-h) * h
+     *
+     * args:
+     *     :(double) ytm1 - the value of y at t-1
+     *     :(std::function<double (double)> dydt - derivative of F 
+     *     :(double) t - timestep of interest
+     *     :(double) h - width of a step 
+     * 
+     * returns:
+     *     :(double) approx F(t)
+     */
+    return ytm1 + dydt(t-h)*h; 
+}
+
--- numerical_integration.hpp
+++ numerical_integration.hpp
@@ -1,9 +1,9 @@
-#ifndef Graph_hpp
-#define Graph_hpp
+#ifndef numerical_integration_hpp
+#define numerical_integration_hpp 
 
 #include <functional>
 
 double euler(double ytm1, std::function<double (double)> dydt, double t, double h); 
 /* computes euler's method: yn = yn-1 + fn * h */
 
-#endif /* Graph_hpp */
+#endif
