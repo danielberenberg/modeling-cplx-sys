5143e5000b35b715d512d3287194e8ac8f3e99e7
Sarah How <sehowerter@gmail.com>
2018-09-27 11:57:50 -0400

    plotupdates

--- numerical.py
+++ numerical.py
@@ -5,16 +5,19 @@ import numpy as np
 import pandas as pd
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
+from matplotlib import rc
+rc('font',**{'family':'serif','serif':['Palatino']})
+rc('text', usetex=True)
 
 n = 3
 
 def species_deriv(X,A):
     """
-    compute the derivative of the position of each of the species in 
+    compute the derivative of the position of each of the species in
     the LK model
 
     args:
-        :X (list) - positions of each xi 
+        :X (list) - positions of each xi
 
     returns:
         :(np.array) - dxi/dt
@@ -38,14 +41,14 @@ def Euler(X,A,h):
     using Euler's method
 
     args:
-        :X (np.array) - the positions of each xi 
+        :X (np.array) - the positions of each xi
         :h (float) - stepsize
 
     returns:
         :(np.array) - appx location of each at ti+1
     """
     dXdt = species_deriv(X,A)
-    
+
     #return np.array(X + np.multiply(h,dXdt))
     return np.array([xi + dXdt[i]*h for i,xi in enumerate(X)],dtype=np.float64)
 
@@ -56,7 +59,7 @@ def Heun(X,A,h):
     using Heun's method
 
     args:
-        :X (np.array) - the positions of each xi 
+        :X (np.array) - the positions of each xi
         :h (float) - stepsize
 
     returns:
@@ -65,15 +68,15 @@ def Heun(X,A,h):
 
     Xp1 = Euler(X,A,h)
     dXp1dt = species_deriv(Xp1,A)
-    dXdt = species_deriv(X,A) 
-    
+    dXdt = species_deriv(X,A)
+
     #return np.array(X + np.multiply((h/2),np.multiply(dXdt, dXp1dt)))
     return np.array([xi + (h/2)*(dXdt[i] + dXp1dt[i]) for i,xi in enumerate(X)],dtype=np.float64)
 
 def approximate(A,initial_positions,h,T=200):
-    # zero out the approximations 
-    Xe = np.zeros((T,n),dtype=np.float64)  
-    Xh = np.zeros((T,n),dtype=np.float64) 
+    # zero out the approximations
+    Xe = np.zeros((T,n),dtype=np.float64)
+    Xh = np.zeros((T,n),dtype=np.float64)
     Xe[0] = initial_positions
     Xh[0] = initial_positions
     for i in range(1, T):
@@ -83,8 +86,8 @@ def approximate(A,initial_positions,h,T=200):
     return Xe, Xh
 
 if __name__ == "__main__":
-    T = 200 # max timestep 
-    n = 3   # number of species 
+    T = 200 # max timestep
+    n = 3   # number of species
 
     alphas = [0.75, 1.2, 1.5] # alphas to iterate over
     stepsizes = [0.1,0.5,1.] # stepsizes to consider
@@ -93,12 +96,13 @@ if __name__ == "__main__":
     A = np.array([[0.5, 0.5, 0.1],
                   [-0.5,-0.1,0.1],
                   [alphas[0],0.1,0.1]])
-    
-    initial_positions = [0.3, 0.2, 0.1] # xi_0 for each species i 
-    
+
+    initial_positions = [0.3, 0.2, 0.1] # xi_0 for each species i
+
     # solve the 3-species system for each h for each alpha
+    k=0
     for h in stepsizes:
-        t = np.arange(0,T,h) 
+        t = np.arange(0,T,h)
         for alpha in alphas:
             print("\rSolving 3-species LK system [a={:.02f},h={:.02f}]".format(alpha,h),end="",flush=True)
             A[2][0] = alpha
@@ -107,12 +111,12 @@ if __name__ == "__main__":
             Xe, Xh = approximate(A,initial_positions,h, T=len(t))
             x1e, x2e, x3e = zip(*Xe)
             x1h, x2h, x3h = zip(*Xh)
-            
+
             fig, axarr = plt.subplots(3,1)
             for i, ax in enumerate(axarr):
-                ax.set_ylabel("$x_{}$".format(i+1))
-                ax.set_xlabel("$t$")
-                plt.suptitle(r"3-Species approximation for $\alpha={},h={}$".format(alpha,h))
+                ax.set_ylabel("$x_{}$".format(i+1),fontsize=14)
+                ax.set_xlabel("$t$",fontsize=14)
+                #plt.suptitle(r"3-Species approximation for $\alpha={},h={}$".format(alpha,h))
 
 
             if (alpha, h) != (1.5,1):
@@ -151,6 +155,10 @@ if __name__ == "__main__":
                 axarr[2].plot(t3e,x3e_p)
                 axarr[2].plot(t,x3h)
                 axarr[2].plot(X_.t,x3,color="k")
-            
-            axarr[0].legend()
-            plt.savefig("figs/3SpeciesApprox{}_{}.pdf".format(alpha,h))
+
+            if k==6:
+                axarr[0].legend(loc='upper right')
+            else:
+                axarr[0].legend().set_visible(False)
+            plt.savefig("figs/3SpeciesApprox{}_{}.pdf".format(int(alpha*100),int(h*10)),bbox_inches='tight')
+            k+=1
