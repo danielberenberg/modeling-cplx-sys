feee6504d6d9f1bcbc68af2023dee181f7cea048
danielberenberg <djberenb@uvm.edu>
2018-10-10 19:52:57 -0400

    changes t dla

--- dla.py
+++ dla.py
@@ -10,6 +10,7 @@ import argparse
 import matplotlib.pyplot as plt
 import imageio
 import os
+import sys
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Diffusion Limited Aggregation")
@@ -80,11 +81,12 @@ def move_walkers(mat):
     walkers_lbld = measure.label(mat, connectivity=1)
     # extract indexes by returning all indices that had a one
     idx = [np.array(np.where(walkers_lbld == label)).T.tolist() for label in np.unique(walkers_lbld) if label]
+    print(idx)
     M = np.zeros(mat.shape)  # new matrix
-    mask = [] 
+    maskX, maskY = [], [] 
     try:  # test if there are clusters > length 2, concatenate each such cluster, split into 2 sep. arrays
-        mask = list(zip(*reduce(lambda x1,x2: x1 + x2, filter(lambda x: len(x) > 1, idx))))
-        M[mask] = 1
+        maskX, maskY = zip(*reduce(lambda x1,x2: x1 + x2, filter(lambda x: len(x) > 1, idx)))
+        M[maskX, maskY] = 1
     except TypeError:  # implies no clusters exist
         pass
     
@@ -95,7 +97,9 @@ def move_walkers(mat):
             3:np.array([1,0])}
     
     # get movers that aren't found in clusters
-    movers = np.array(list(filter(lambda x: x not in mask, map(lambda x: x[0], idx))))
+    nonmovers = list(zip(maskX, maskY))
+    movers = list(filter(lambda x: x not in nonmovers, idx))
+    sys.exit() 
     # select moves for each mover
     moves = np.array(list(map(lambda x: NSEW[x], np.random.randint(0,high=4,size=len(movers)))))
     # generate the next positions for the walkers to move with periodic boundaries
@@ -115,7 +119,9 @@ def plot_matrix(A, name):
 
 if __name__ == "__main__":
     args = parse_args().parse_args()
+    
     L = len(str(args.generations))
+
     fmt = "\r[{:%d" % L + "d}]"
     outfile_str = os.path.join(args.frame_directory, "GifPhoto__{:05d}.png") 
     os.makedirs(args.frame_directory, exist_ok=True)
@@ -123,7 +129,6 @@ if __name__ == "__main__":
     spawn_walkers(M, N=args.n_walkers)
     filenames = []
 
-    print(args.outfile)
     for g in range(args.generations):
         print(fmt.format(g),end="",flush=True)
         M = move_walkers(M)
